ba13a2d23425bb9e50b851becedfe10fee9ac821
jc-programs <jose@gueto.com>
2024-11-29 22:22:00 +0100

    changed tag on build-backend.sh

--- PRA04_ANSWER/build-backend.sh
+++ PRA04_ANSWER/build-backend.sh
@@ -5,7 +5,7 @@ while IFS== read -r key value; do
 done <.env
 
 docker build \
-    --tag pra03/backend-books:1.0 \
+    --tag pra04/backend-books:1.0 \
     --file backend.Dockerfile \
     --build-arg springFolder="${SPRING_DIR}" \
     --build-arg springJar="${SPRING_JAR}" \
